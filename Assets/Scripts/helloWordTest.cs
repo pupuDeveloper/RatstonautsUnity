f24359012abde036bf14e94ec77d7e84c6110592
saimivilkkila <saimi.vilkkila@gmail.com>
2023-04-13 14:38:14 +0300

    just testing if commiting and stuff works

---
+++ Assets/Scripts/helloWordTest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloWordTest : MonoBehaviour
+{
+
+    private void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+}

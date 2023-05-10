c718cabbd69e3fb9d7c7933d6761f6599d9d2533
saimivilkkila <saimi.vilkkila@gmail.com>
2023-05-10 04:48:15 +0300

    added sound to simon says buttons

---
+++ Assets/Scripts/CockpitMinigame/simonSaysButtons.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simonSaysButtons : MonoBehaviour
+{
+    public AudioSource soundPlayer;
+    public AudioSource soundPlayer2;
+    public AudioSource soundPlayer3;
+    public AudioSource soundPlayer4;
+    public AudioSource soundPlayer5;
+    public AudioSource soundPlayer6;
+
+
+    public void playSoundEffect()
+    {
+        soundPlayer.Play();
+    }
+    public void playSoundEffect2()
+    {
+        soundPlayer2.Play();
+    }
+    public void playSoundEffect3()
+    {
+        soundPlayer3.Play();
+    }
+    public void playSoundEffect4()
+    {
+        soundPlayer4.Play();
+    }
+    public void playSoundEffect5()
+    {
+        soundPlayer5.Play();
+    }
+    public void playSoundEffect6()
+    {
+        soundPlayer6.Play();
+    }
+}

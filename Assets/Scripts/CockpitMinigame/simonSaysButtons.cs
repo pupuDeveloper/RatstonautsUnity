c1ee21a2e896ed40daf13a7ea6b1afccdb268509
saimivilkkila <saimi.vilkkila@gmail.com>
2023-05-13 21:56:24 +0300

    a bit of progress

--- Assets/Scripts/CockpitMinigame/simonSaysButtons.cs
+++ Assets/Scripts/CockpitMinigame/simonSaysButtons.cs
@@ -18,7 +18,8 @@ public class simonSaysButtons : MonoBehaviour
     private Button simonButton5;
     private Button simonButton6;
     private bool turn;
-    private string[] compMoves = new string[5];
+    private string compMoves = "";
+    private string playerMoves = "";
 
     private void Start()
     {
@@ -47,66 +48,121 @@ public class simonSaysButtons : MonoBehaviour
 
     private void Update()
     {
-        if (!turn)
+        if (turn == false)
         {
-
+            StartCoroutine(simonCoroutine());
+            turn = true;
+        }
+        else
+        {
+        simonButton1.enabled = true;
+        simonButton2.enabled = true;
+        simonButton3.enabled = true;
+        simonButton4.enabled = true;
+        simonButton5.enabled = true;
+        simonButton6.enabled = true;
         }
     }
 
     IEnumerator simonCoroutine()
     {
+        Debug.Log("got in");
+        simonButton1.enabled = true;
+        simonButton2.enabled = true;
+        simonButton3.enabled = true;
+        simonButton4.enabled = true;
+        simonButton5.enabled = true;
+        simonButton6.enabled = true;
         for (int i = 0; i <= 5; i++)
         {
             int randButton = Random.Range(1, 6);
-            compMoves[i] = randButton;
+            compMoves = string.Concat(compMoves, randButton);
             yield return new WaitForSeconds(1);
             switch (randButton)
             {
                 case 1:
                 simonButton1.onClick.Invoke();
+                Debug.Log("1 pressed");
                 break;
                 case 2:
                 simonButton2.onClick.Invoke();
+                Debug.Log("2 pressed");
                 break;
                 case 3:
                 simonButton3.onClick.Invoke();
+                Debug.Log("3 pressed");
                 break;
                 case 4:
                 simonButton4.onClick.Invoke();
+                Debug.Log("4 pressed");
                 break;
                 case 5:
                 simonButton5.onClick.Invoke();
+                Debug.Log("5 pressed");
                 break;
                 case 6:
                 simonButton6.onClick.Invoke();
+                Debug.Log("6 pressed");
                 break;
             }
         }
+        simonButton1.enabled = false;
+        simonButton2.enabled = false;
+        simonButton3.enabled = false;
+        simonButton4.enabled = false;
+        simonButton5.enabled = false;
+        simonButton6.enabled = false;
+        Debug.Log(playerMoves);
+        Debug.Log(compMoves);
     }
 
 
     public void playSoundEffect()
     {
         soundPlayer.Play();
+        if (turn)
+        {
+            playerMoves = string.Concat(playerMoves, 1);
+        }
     }
     public void playSoundEffect2()
     {
         soundPlayer2.Play();
+        if (turn)
+        {
+            playerMoves = string.Concat(playerMoves, 2);
+        }
     }
     public void playSoundEffect3()
     {
         soundPlayer3.Play();
+        if (turn)
+        {
+            playerMoves = string.Concat(playerMoves, 3);
+        }
     }
     public void playSoundEffect4()
     {
         soundPlayer4.Play();
+        if (turn)
+        {
+            playerMoves = string.Concat(playerMoves, 4);
+        }
     }
     public void playSoundEffect5()
     {
         soundPlayer5.Play();
+        if (turn)
+        {
+            playerMoves = string.Concat(playerMoves, 5);
+        }
     }
     public void playSoundEffect6()
     {
         soundPlayer6.Play();
+        if (turn)
+        {
+            playerMoves = string.Concat(playerMoves, 6);
+        }
     }
 }

fbc309414665c60c848f26bb2b371817a41e8e8b
saimivilkkila <saimi.vilkkila@gmail.com>
2024-05-29 04:10:57 +0300

    project start stuff

---
+++ Assets/code/managers/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("GameManager is NULL");
+            }
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        // Destroy dublicate instances
+        if (_instance != null)
+        {
+            _instance = this;
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+            return; // required bcs destroy call is not instant
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}

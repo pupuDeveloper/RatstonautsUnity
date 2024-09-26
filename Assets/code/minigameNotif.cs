f74f000a008173c0377189bf050f13d764783826
saimivilkkila <saimi.vilkkila@gmail.com>
2024-09-26 17:56:14 +0300

    small ui things

---
+++ Assets/code/minigameNotif.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minigameNotif : MonoBehaviour
+{
+    [SerializeField] private GameObject notif1, notif2, notif3, notif4;
+
+    void Start()
+    {
+        checkIfGameplayAvailable();
+    }
+
+    public void checkIfGameplayAvailable()
+    {
+        if (GameManager.Instance.cockpitBoostOn == false)
+        {
+            notif1.SetActive(true);
+        }
+        else
+        {
+            notif1.SetActive(false);
+        }
+
+        if (GameManager.Instance.gardenBoostOn == false)
+        {
+            notif2.SetActive(true);
+        }
+        else
+        {
+            notif2.SetActive(false);
+        }
+
+        if (GameManager.Instance.turretsBoostOn == false)
+        {
+            notif3.SetActive(true);
+        }
+        else
+        {
+            notif3.SetActive(false);
+        }
+
+        if (GameManager.Instance.foodGenBoostOn == false)
+        {
+            notif4.SetActive(true);
+        }
+        else
+        {
+            notif4.SetActive(false);
+        }
+    }
+}

7865cdf81adcb22aebcd5cfa9981e4276bf8946d
saimivilkkila <saimi.vilkkila@gmail.com>
2024-10-27 22:10:07 +0200

    toggling music and SFX on and off works

--- Assets/code/managers/AudioManager.cs
+++ Assets/code/managers/AudioManager.cs
@@ -26,12 +26,13 @@ public class AudioManager : MonoBehaviour
             s.source.clip = s.clip;
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
         }
     }
 
     private void Start()
     {
-        
+        Play("BGsong");
     }
 
     public void Play(string name)
--- Assets/code/settings/settingsUI.cs
+++ Assets/code/settings/settingsUI.cs
@@ -12,4 +12,39 @@ public class settingsUI : MonoBehaviour
     {
         AudioManager.instance.Play("UI1");
     }
+    public void muteSFX()
+    {
+        foreach (Sound s in AudioManager.instance.sounds)
+        {
+            if (s.type == Sound.Type.sfx)
+            {
+                if (s.source.volume == 0f)
+                {
+                    s.source.volume = 1f;
+                }
+                else
+                {
+                    s.source.volume = 0f;
+                }
+            }
+        }
+    }
+
+    public void muteMusic()
+    {
+        foreach (Sound s in AudioManager.instance.sounds)
+        {
+            if (s.type == Sound.Type.music)
+            {
+                if (s.source.volume == 0f)
+                {
+                    s.source.volume = 1f;
+                }
+                else
+                {
+                    s.source.volume = 0f;
+                }
+            }
+        }
+    }
 }

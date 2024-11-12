44e7f5667fd1de33288a59ae7bafbcd1b4b28f00
saimivilkkila <saimi.vilkkila@gmail.com>
2024-11-12 12:20:09 +0200

    started work on showing milestones etc

---
+++ Assets/code/miscUIstuffMainScene/roomInfoItems/cockPitMilestones.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cockPitMilestones : MonoBehaviour
+{
+    private Transform[] gridItems;
+
+    [Tooltip("milestone array")]
+    public milestoneInfo[] _milestoneArray;
+    private void Start()
+    {
+        gridItems = transform.GetChild(0).transform.GetChild(0).GetComponentsInChildren<Transform>();
+        _milestoneArray = transform.GetChild(0).transform.GetChild(0).GetComponentsInChildren<milestoneInfo>();
+    }
+    private void scaleItems()
+    {
+        foreach (Transform item in gridItems)
+        {
+            item.GetComponent<scaleLayoutItems>().Scale(item.transform.parent.GetComponent<RectTransform>().rect.width, item.parent.GetComponent<RectTransform>().rect.height/8);
+        }
+    }
+    private void setInfo()
+    {
+        for (int i = 0; i < _milestoneArray.Length; i++)
+        {
+            gridItems[i].GetChild(0).GetComponent<TMP_Text>
+        }
+    }
+}
---
+++ Assets/code/miscUIstuffMainScene/roomInfoItems/milestoneInfo.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[System.Serializable]
+public class milestoneInfo
+{
+    [Tooltip("milestone LVL")]
+    public int lvl;
+    [Tooltip("milestone description")]
+    public string description;
+
+}

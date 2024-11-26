b1dcce0c1650d89fdef9891d67e362e264f09555
saimivilkkila <saimi.vilkkila@gmail.com>
2024-11-26 18:45:44 +0200

    fixed ui position on all screen sizes

--- Assets/code/scalingScripts/ScaleToFitScreen.cs
+++ Assets/code/scalingScripts/ScaleToFitScreen.cs
@@ -9,8 +9,7 @@ public class ScaleToFitScreen : MonoBehaviour
     [SerializeField] GameObject bottomBar;
     float worldScreenHeight;
     float worldScreenWidth;
-    float xBuffer;
-    float yBuffer;
+    float height;
 
     private void Start()
     {
@@ -21,23 +20,27 @@ public class ScaleToFitScreen : MonoBehaviour
 
         transform.localScale = new Vector3(((canvas.GetComponent<RectTransform>().rect.width) / sr.sprite.bounds.size.x) * 1.1f, ((canvas.GetComponent<RectTransform>().rect.height) / sr.sprite.bounds.size.y) * 1.1f, 1);
         GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
-        float height = canvas.GetComponent<RectTransform>().rect.height - Mathf.Abs(bottomBar.GetComponent<RectTransform>().rect.height) - Mathf.Abs(topBar.GetComponent<RectTransform>().rect.height);
+        height = canvas.GetComponent<RectTransform>().rect.height - Mathf.Abs(bottomBar.GetComponent<RectTransform>().rect.height) - Mathf.Abs(topBar.GetComponent<RectTransform>().rect.height);
         layoutScaleScript.Scale(canvas.GetComponent<RectTransform>().rect.width, height);
     }
     public float getX()
     {
         return canvas.GetComponent<RectTransform>().rect.width;
     }
-    public float getXBuffer()
-    {
-        return xBuffer;
-    }
-    public float getYBuffer()
+    public float GetTopBarY()
     {
         return topBar.GetComponent<RectTransform>().rect.height;
+    }
+    public float getBotBarY()
+    {
+        return bottomBar.GetComponent<RectTransform>().rect.height;
     }
     public float getY()
     {
         return canvas.GetComponent<RectTransform>().rect.height;
+    }
+    public float getheight()
+    {
+        return height;
     }
 }
